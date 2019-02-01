787757e058d981909bc706a813c77ed814d94d2c
CGunaratne <chatura.gunaratne@cambio.lk>
2019-02-01 13:26:18 +0530

    product row changes added

--- src/app/product-row/product-row.component.ts
+++ src/app/product-row/product-row.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product-row',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-row.component.css']
 })
 export class ProductRowComponent implements OnInit {
+
+  @Input() product: Product;
 
   constructor() { }
 
